d5233946c34bde47b8178902fa628f481ae17a18
Sourabh Ghorpade <sourabh.ghorpade@gmail.com>
2022-12-13 16:16:59 +0530

    Initial Commit.

---
+++ lib/ruby/interview/problem.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "problem/version"
+
+module Ruby
+  module Interview
+    module Problem
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
