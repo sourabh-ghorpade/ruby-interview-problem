11b98c16a872469b0e208b28674a45253d142ddf
pratyush <guptapratyush98@gmail.com>
2022-12-22 16:34:07 +0530

    - write first iteration of the helper function to isolate amount calculation logic per rental

--- lib/movie_rental/movie_rental.rb
+++ lib/movie_rental/movie_rental.rb
@@ -3,20 +3,20 @@
 require_relative "version"
 
 # Sample input
-# customer=
-# {
-#   name: "martin",
-#   rentals: [
-#     { movie_id: "F001", days: 3 },
-#     { movie_id: "F002", days: 1 }
-#   ]
-# }
+customer=
+{
+  name: "martin",
+  rentals: [
+    { movie_id: "F001", days: 3 },
+    { movie_id: "F002", days: 1 }
+  ]
+}
 
-# movies=
-# {
-#   "F001" => { title: "Ran",                     code: "regular" },
-#   "F002" => { title: "Trois Couleurs: Bleu",     code: "regular" }
-# }
+movies=
+{
+  "F001" => { title: "Ran",                     code: "regular" },
+  "F002" => { title: "Trois Couleurs: Bleu",     code: "regular" }
+}
 
 # Expected output
 # "Rental Record for martin\n\tRan\t3.5\n\tTrois Couleurs: Bleu\t2\nAmount owed is 5.5\nYou earned 2 frequent renter points\n"
@@ -31,20 +31,22 @@ module MovieRental
       movie = movies[r[:movie_id]]
       this_amount = 0
 
-      case movie[:code]
-      when "regular"
-        this_amount = 2
-        if r[:days] > 2
-          this_amount += (r[:days] - 2) * 1.5
-        end
-      when "new"
-        this_amount = r[:days] * 3
-      when "childrens"
-        this_amount = 1.5
-        if r[:days] > 3
-          this_amount += (r[:days] - 3) * 1.5
-        end
-      end
+      this_amount = MovieRental.calculate_movie_amount(movie, this_amount, r)
+
+      # case movie[:code]
+      # when "regular"
+      #   this_amount = 2
+      #   if r[:days] > 2
+      #     this_amount += (r[:days] - 2) * 1.5
+      #   end
+      # when "new"
+      #   this_amount = r[:days] * 3
+      # when "childrens"
+      #   this_amount = 1.5
+      #   if r[:days] > 3
+      #     this_amount += (r[:days] - 3) * 1.5
+      #   end
+      # end
 
       frequent_renter_points += 1
       if movie[:code] == "new" && r[:days] > 2
@@ -60,4 +62,25 @@ module MovieRental
 
     result
   end
+
+  def self.calculate_movie_amount(movie, this_amount, r)
+    case movie[:code]
+    when "regular"
+      this_amount = 2
+      if r[:days] > 2
+        this_amount += (r[:days] - 2) * 1.5
+      end
+    when "new"
+      this_amount = r[:days] * 3
+    when "childrens"
+      this_amount = 1.5
+      if r[:days] > 3
+        this_amount += (r[:days] - 3) * 1.5
+      end
+    end
+
+    this_amount
+  end
 end
+
+puts MovieRental.statement(customer, movies)
